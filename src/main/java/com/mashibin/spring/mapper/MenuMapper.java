277888ff0eed7aebf65c10c1a40bd65070e5a4c8
Gaia <1531772666@qq.com>
2020-03-02 02:27:20 +0800

    first committed project

---
+++ src/main/java/com/mashibin/spring/mapper/MenuMapper.java
@@ -0,0 +1,33 @@
+package com.mashibin.spring.mapper;
+
+import com.mashibin.spring.entity.Menu;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MenuMapper {
+    long countByExample(MenuExample example);
+
+    int deleteByExample(MenuExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Menu record);
+
+    int insertSelective(Menu record);
+
+    List<Menu> selectByExample(MenuExample example);
+
+    Menu selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Menu record, @Param("example") MenuExample example);
+
+    int updateByExample(@Param("record") Menu record, @Param("example") MenuExample example);
+
+    int updateByPrimaryKeySelective(Menu record);
+
+    int updateByPrimaryKey(Menu record);
+}
---
+++ src/test/java/com/mashibin/spring/SpringBootMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibin.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
